ba4a4cf4b9bef65c995d09ba25d4ee3f381c49d9
Faythn <fkioko790@gmail.com>
2025-08-11 21:48:18 +0300

    correct

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    # Login view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # Logout view
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # ... any other urls
 ]
+
