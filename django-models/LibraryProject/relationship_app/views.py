050863008215d6552d0b8b8ba5a3d1246828f931
Faythn <fkioko790@gmail.com>
2025-08-11 11:28:31 +0300

    Correct

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,20 @@
+# relationship_app/views.py
+from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views.generic.detail import DetailView
 
+# Function-based view (plain text for the checker)
 def list_books(request):
     books = Book.objects.select_related('author').all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    # Build plain text output (for the checker)
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
 
-
-
-from django.views.generic.detail import DetailView
-from .models import Library
-
+# Class-based view (Library details)
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
 
