be3e48b44bb37cc725c8069090ebb78806f77f9c
Faythn <fkioko790@gmail.com>
2025-09-01 14:31:46 +0300

    new file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,19 +4,17 @@ from rest_framework.authtoken.models import Token
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # ✅ checker looks for this
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = get_user_model()
         fields = ("username", "email", "password")
 
     def create(self, validated_data):
-        # ✅ checker looks for get_user_model().objects.create_user
         user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
         )
-        # ✅ checker looks for Token.objects.create
         Token.objects.create(user=user)
         return user
