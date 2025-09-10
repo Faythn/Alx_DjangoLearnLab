b8156520f1d068740b53851a32e791dbf203fbbb
Faythn <fkioko790@gmail.com>
2025-09-10 11:09:11 +0300

    New function

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView
